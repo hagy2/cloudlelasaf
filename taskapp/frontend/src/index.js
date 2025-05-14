fbd1c551ce0eb7030f011b06dd688fec021cd993
hagy2 <hagerwasef@gmail.com>
2025-05-14 21:37:17 +0300

    s

--- taskapp/frontend/src/index.js
+++ taskapp/frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'C:\Users\Hager\OneDrive\Desktop\clo\taskapp\frontend\src\app.js';
+import App from 'taskapp/frontend/src/app.js';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
